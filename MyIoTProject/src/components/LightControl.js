b683023ccfb73160e8629a6ea917c9b2bf913eba
Maria Júlia <maju.mariajuliaaraujo@gmail.com>
2026-05-25 11:08:20 -0300

    Criando LightControl

---
+++ MyIoTProject/src/components/LightControl.js
@@ -0,0 +1,35 @@
+import React from 'react';
+import {StyleSheet, View, Text, TouchableOpacity} from 'react-native';
+import {MaterialCommunityIcons as Icon} from '@expo/vector-icons';
+
+export default function LightControl({ isLightON, onToggle}) {
+    return (
+        <View style={StyleSheet.card}>
+            <TouchableOpacity onPress={onToggle}>
+                <Icon
+                 name={isLightON ? 'lightbulb-on' : 'lightbulb-outline'}
+                 size={100}
+                 color={isLightON ? '#F1C40F' : '#555'}
+                />
+            </TouchableOpacity>
+            <Text style={styles.label}> {isLightON ? 'Luz Ligada' : 'Luz Desligada'} </Text>
+        </View>
+    );
+}
+
+const styles = StyleSheet.create({
+    card: {
+    backgroundColor: '#1E1E1E',
+    padding: 30,
+    borderRadius: 20,
+    alignItems: 'center',
+    width: '100%',
+    marginBottom: 20,
+},
+
+    label: {
+        color: '#AAA',
+        marginTop: 10,
+        fontSize: 14,
+    },
+});
